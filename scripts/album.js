a3246833ec5a85064da734bc145083dfc73c5961
Evan Snow <snow@evans-mbp.attlocal.net>
2017-04-26 20:27:56 -0500

    Checkpoint 18: Complete

--- scripts/album.js
+++ scripts/album.js
@@ -37,7 +37,47 @@ var createSongRow = function(songNumber, songName, songLength) {
       + '</tr>'
       ;
  
-    return $(template);
+    var $row = $(template);
+    
+    var clickHandler = function() {
+	var songNumber = $(this).attr('data-song-number');
+
+	if (currentlyPlayingSong != null) {
+		var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
+		currentlyPlayingCell.html(currentlyPlayingSong);
+	}
+	if (currentlyPlayingSong != songNumber) {
+		$(this).html(pauseButtonTemplate);
+		currentlyPlayingSong = songNumber;
+	} 
+    else if (currentlyPlayingSong == songNumber) {
+		$(this).html(playButtonTemplate);
+		currentlyPlayingSong = null;
+	}
+};
+
+    
+    var onHover = function(event) {
+        var songNumberCell = $(this).find('.song-item-number');
+        var songNumber = songNumberCell.attr('data-song-number');
+
+        if (songNumber != currentlyPlayingSong) {
+            songNumberCell.html(playButtonTemplate);
+        }
+    };
+
+    var offHover = function(event) {
+        var songNumberCell = $(this).find('.song-item-number');
+        var songNumber = songNumberCell.attr('data-song-number');
+
+        if (songNumber != currentlyPlayingSong) {
+            songNumberCell.html(songNumber);
+        }
+    };
+    
+    $row.find('.song-item-number').click(clickHandler);
+    $row.hover(onHover, offHover);
+    return $row;
 }; 
 
 var setCurrentAlbum = function(album) {
@@ -62,62 +102,13 @@ var setCurrentAlbum = function(album) {
     }
 };
 
-var clickHandler = function(targetElement) {
-    var songItem = getSongItem(targetElement); 
-      if (currentlyPlayingSong === null) {
-        songItem.innerHTML = pauseButtonTemplate;
-        currentlyPlayingSong = songItem.getAttribute('data-song-number');
-  } 
-    else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
-        songItem.innerHTML = playButtonTemplate;
-          currentlyPlayingSong = null;
-  } 
-    else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
-       var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
-         currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
-          songItem.innerHTML = pauseButtonTemplate;
-           currentlyPlayingSong = songItem.getAttribute('data-song-number');
-     }
-};
-
-var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
-var songRows = document.getElementsByClassName('album-view-song-item');
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 var currentlyPlayingSong = null;
 
-window.onload = function() {
+$(document).ready(function() {
   setCurrentAlbum(albumPicasso);
- };
-
-songListContainer.addEventListener('mouseover', function(event) {
-    
-    if (event.target.parentElement.className === 'album-view-song-item') {
-        
-        event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
-        var songType = getSongType(event.target);
-    
-    if (songType.getAttribute('data-song-number') != currentlyPlayingSong) {
-        songType.innerHTML = playButtonTemplate;
-      }
-    }
 });
-
-for (var i = 0; i < songRows.length; i++) {
-    
-    songRows[i].addEventListener('mouseleave', function(event) {
-      var songsItems = getSongItems(event.target);
-        var songItemNumbers = songItems.getAttribute('data-song-number');
- 
-      if (songItemNumbers !== currentlyPlayingSong) {
-          songItems.innerHTML = songItemNumbers;
-     }
-  });
-}
-
-songRows[i].addEventListener('click', function(event) {
-     clickHandler(event.target);          
-    });
 
 var parentName = function(element, eClass) {
     if (element) {
@@ -127,7 +118,7 @@ var parentName = function(element, eClass) {
         }
         return eParent;
     }
-};
+});
 
 var newSong = function(element) {
     switch (element.className) {
